a73ff021c3d5ca02fa79843a3ec474e9c36b79ee
Mary Gwen Susa <Mary Gwen G. Susa>
2022-11-19 14:05:32 +0800

    implemented choosing a certain page and extracting its texts to read aloud

--- audiobook.py
+++ audiobook.py
@@ -6,5 +6,7 @@ pdfREader = PyPDF2.PdfFileReader(book) #reads the PDF file linked
 pages = pdfREader.numPages #counts the number of pages of the PDF file linked
 print(pages)
 speaker = pyttsx3.init() #initialize
-speaker.say('Hey, can you hear me?')
+page = pdfREader.getPage(3) # 0 indexing
+text = page.extractText() #will extract the text from the chosen page
+speaker.say(text)
 speaker.runAndWait()
