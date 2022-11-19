c310e7aa71da09de7c743d4fc3a849c7e8f3bdf8
Mary Gwen Susa <Mary Gwen G. Susa>
2022-11-19 13:52:46 +0800

    enabled the PyPDF2 package to open the pdf and count its pages; had an error kept on forgetting to save in a variable the functions/tasks

--- audiobook.py
+++ audiobook.py
@@ -1,4 +1,10 @@
 import pyttsx3
+import PyPDF2
+
+book = open('Kohlbergian_Analysis_of_the_Moral_Reasoning_in_Lin.pdf', 'rb') # rb - read-only in binary format
+pdfREader = PyPDF2.PdfFileReader(book) #reads the PDF file linked
+pdfREader.numPages #counts the number of pages of the PDF file linked
+print(pages)
 speaker = pyttsx3.init() #initialize
-speaker.say('Hey, can you hear me!')
+speaker.say('Hey, can you hear me?')
 speaker.runAndWait()
