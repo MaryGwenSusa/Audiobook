b3575eed3b0f72ee2fee0e5fb09f82e2fb68c9fa
Mary Gwen Susa <Mary Gwen G. Susa>
2022-11-19 13:53:25 +0800

    error fixed

--- audiobook.py
+++ audiobook.py
@@ -3,7 +3,7 @@ import PyPDF2
 
 book = open('Kohlbergian_Analysis_of_the_Moral_Reasoning_in_Lin.pdf', 'rb') # rb - read-only in binary format
 pdfREader = PyPDF2.PdfFileReader(book) #reads the PDF file linked
-pdfREader.numPages #counts the number of pages of the PDF file linked
+pages = pdfREader.numPages #counts the number of pages of the PDF file linked
 print(pages)
 speaker = pyttsx3.init() #initialize
 speaker.say('Hey, can you hear me?')
