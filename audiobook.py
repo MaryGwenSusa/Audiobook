e2755f0858bf399bf8baf210af212bf4c836644c
Mary Gwen Susa <Mary Gwen G. Susa>
2022-11-19 14:14:55 +0800

    adusted its reading speed

--- audiobook.py
+++ audiobook.py
@@ -5,9 +5,14 @@ book = open('Kohlbergian_Analysis_of_the_Moral_Reasoning_in_Lin.pdf', 'rb') # rb
 pdfREader = PyPDF2.PdfFileReader(book) #reads the PDF file linked
 pages = pdfREader.numPages #counts the number of pages of the PDF file linked
 print(pages)
+
 speaker = pyttsx3.init() #initialize
+rate = speaker.getProperty('rate')
+speaker.setProperty('rate', 180) #adjusted the speed of reading since its default 200 is too fast
+print(rate)
+
 for num in range(2, 3): #freely choose the range of pages you want to recall or read aloud
-    page = pdfREader.getPage() # 0 indexing; can remove the for loop and just choose a single page as a parameter
+    page = pdfREader.getPage(2) # 0 indexing; can remove the for loop and just choose a single page as a parameter
     text = page.extractText() #will extract the text from the chosen page
     speaker.say(text)
     speaker.runAndWait()
